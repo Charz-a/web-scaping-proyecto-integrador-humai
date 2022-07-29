4159e9bd0800746ed67d0fb3c50761712916e8e0
tomasbatti <tomasbattistella@gmail.com>
2022-07-29 16:00:11 -0300

    subo scraper con cambios final, falta cron nada mas

--- source/scraper.py
+++ source/scraper.py
@@ -1,8 +1,12 @@
 from calendar import week
 import requests
 import lxml.html
+import os
+from datetime import datetime
+import unicodedata
 
-import csv_test
+import csv_test as manage_csv
+
 
 def get_json_page(page_num: int):
     url = 'https://disfrutemosba.com/api/search'
@@ -16,65 +20,15 @@ def get_json_page(page_num: int):
            'sec-ch-ua-platform':'"Linux"'}
 
     response = requests.request("POST",url, headers=headers,data = payload)
+
     return response.json()
-
-def get_data(page, places):
-    #ver que hacemos aca vio
-    for activity in page['activities']['items']:
-        activity_id = activity['id']
-        print("Actividad con id: " + str(activity_id))
-        activty_name = activity['activity']['name']
-        activity_url = "https://disfrutemosba.buenosaires.gob.ar/lugares/" + activity['activity']['slug']
-        dates = get_dates(activity_url)
-        activity_date = dates[0]
-        activity_times = dates[1]
-        try:
-            activity_place = places[int(activity['activity']['kind_of_place_id'])]
-        except:
-            try:
-                activity_place = places[int(activity['activity']['places'][0]['kind_of_place_id'])]
-            except:
-                activity_place = "NULL"
-
-        try:
-            activity_district = activity['activity']['district']['name']
-        except:
-            activity_district = "NULL"
-        #to do: activity_phone?
-        #to do: activity_mail?
-
-        try:
-            activity_address = activity['activity']['address']
-        except:
-            activity_address = "NULL"
-
-        activity_description = "NULL"
-        for modules in activity['activity']['modules']:
-            if modules['type'] == 'Paragraph':
-                activity_description = modules['metadata'][0]['value'] #to do: parsear? - ver si no tiene se puede poner otra descripcion
-        #escribir en csv
-        data_arr = [activity_id,activty_name,activity_url,activity_date,activity_times,activity_place,activity_district,activity_address,activity_description]
-        csv_test.write_csv(data_arr)
-
-def evaluate_data():
-    page = get_json_page(1)
-    places = get_places(page)
-    get_data(page, places)
-    #lastPageNum = int(page['activities']['pagination']['last_page'])
-    lastPageNum = 3 #cambiar hardcodeo
-    for pageNum in range(lastPageNum + 1):
-        if pageNum > 1:
-            print("Script currently in page: "+ str(pageNum))
-            page = get_json_page(pageNum)
-            get_data(page, places)
-
-            
 
 def get_places(page):
     dictionary_places = {}
 
     for place in page['kind_of_places']:
        dictionary_places[place['id']] = place['name']
+
     return dictionary_places
 
 def get_dates(url):
@@ -96,12 +50,82 @@ def get_dates(url):
     if not day_arr:
         day_arr = "NULL"
     else:
-        day_arr = set(day_arr)
+        day_arr_final = []
+        for i in day_arr:
+            day_arr_final.append(strip_accents(i))
+        day_arr = set(day_arr_final)
+
     if not time_arr: 
         time_arr = "NULL"
     else:
         time_arr = set(time_arr)
+
     return day_arr,time_arr
 
+def remove_html_tags(text):
+    import re
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
 
-evaluate_data()
+def strip_accents(texto):
+   return ''.join(c for c in unicodedata.normalize('NFD', texto)
+                  if unicodedata.category(c) != 'Mn')
+
+
+def get_activity(page, places):
+    #ver que hacemos aca vio
+    for activity in page['activities']['items']:
+        activity_id = activity['id']
+        print("Actividad con id: " + str(activity_id))
+        activty_name = strip_accents(activity['activity']['name'])
+        activity_url = "https://disfrutemosba.buenosaires.gob.ar/lugares/" + activity['activity']['slug']
+        dates = get_dates(activity_url)
+        activity_date = dates[0]
+        activity_times = dates[1]
+        try:
+            activity_place = strip_accents(places[int(activity['activity']['kind_of_place_id'])])
+        except:
+            try:
+                activity_place = strip_accents(places[int(activity['activity']['places'][0]['kind_of_place_id'])])
+            except:
+                activity_place = "NULL"
+
+        try:
+            activity_district = strip_accents(activity['activity']['district']['name'])
+        except:
+            activity_district = "NULL"
+
+        try:
+            activity_address = strip_accents(activity['activity']['address'])
+        except:
+            activity_address = "NULL"
+
+        activity_description = "NULL"
+        for modules in activity['activity']['modules']:
+            if modules['type'] == 'Paragraph':
+                activity_description = strip_accents(remove_html_tags(modules['metadata'][0]['value']))
+        #escribir en csv
+        data_arr = [activity_id,activty_name,activity_url,activity_date,activity_times,activity_place,activity_district,activity_address,activity_description]
+        manage_csv.write_csv(data_arr)
+
+def push_csv_result():
+    last_csv_path = manage_csv.get_last_csv()
+    os.system("cd ../data; git add "+last_csv_path +"; git commit -m 'subo ultimo archivo csv generado' "+last_csv_path+";git push origin develop;")
+    
+
+def scrap_activities():
+    print("Script currently in page: 1")
+    page = get_json_page(1)
+    places = get_places(page)
+    get_activity(page, places)
+    lastPageNum = int(page['activities']['pagination']['last_page'])
+    
+    for pageNum in range(lastPageNum + 1):
+        if pageNum > 1:
+            print("Script currently in page: "+ str(pageNum))
+            page = get_json_page(pageNum)
+            get_activity(page, places)
+
+    push_csv_result()
+
+scrap_activities()
